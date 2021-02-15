ec9b8649f6da5049fb35b15657e49370482694a0
joyce04 <glee8804@gmail.com>
2021-02-15 02:57:02 -0800

    add setting view

--- lib/views/setting.dart
+++ lib/views/setting.dart
@@ -1,3 +1,5 @@
+import 'package:anxi_pro/color_scheme.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class Setting extends StatefulWidget {
@@ -8,6 +10,140 @@ class Setting extends StatefulWidget {
 class _SettingState extends State<Setting> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+        padding: EdgeInsets.only(left: 16, top: 25, right: 16),
+        child: ListView(
+          children: [
+            Text(
+              "Settings",
+              style: TextStyle(fontSize: 25, fontWeight: FontWeight.w500),
+            ),
+            SizedBox(
+              height: 40,
+            ),
+            Row(
+              children: [
+                Icon(
+                  Icons.person,
+                  color: purple_2,
+                ),
+                SizedBox(
+                  width: 8,
+                ),
+                Text(
+                  "Account",
+                  style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                )
+              ],
+            ),
+            Divider(
+              height: 15,
+              thickness: 2,
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            _buildAccountOptionRow(context, "Change password"),
+            _buildAccountOptionRow(context, "Notification"),
+            _buildAccountOptionRow(context, "Menu settings"),
+            _buildAccountOptionRow(context, "Privacy"),
+            SizedBox(
+              height: 49,
+            ),
+            Row(
+              children: [
+                Icon(Icons.volume_up_rounded, color: purple_2),
+                SizedBox(
+                  width: 8,
+                ),
+                Text(
+                  "Notifications",
+                  style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                )
+              ],
+            ),
+            Divider(
+              height: 15,
+              thickness: 2,
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            _buildNotificationOptionRow("Push Notifications", true),
+            SizedBox(
+              height: 50,
+            )
+          ],
+        ));
+  }
+
+  _buildNotificationOptionRow(String title, bool isActive) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        Text(
+          title,
+          style: TextStyle(fontSize: 18, fontWeight: FontWeight.w500, color: Colors.black38),
+        ),
+        Transform.scale(
+          scale: 0.7,
+          child: CupertinoSwitch(
+            value: isActive,
+            onChanged: (bool val) {},
+          ),
+        )
+      ],
+    );
+  }
+
+  GestureDetector _buildAccountOptionRow(BuildContext context, String title) {
+    return GestureDetector(
+      onTap: () {
+        showDialog(
+            context: context,
+            builder: (BuildContext context) {
+              return AlertDialog(
+                title: Text(title),
+                content: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    Text("Option 1"),
+                    Text("Option 2"),
+                    Text("Option 3"),
+                  ],
+                ),
+                actions: [
+                  FlatButton(
+                      onPressed: () {
+                        Navigator.of(context).pop();
+                      },
+                      child: Text("Close")),
+                ],
+              );
+            });
+      },
+      child: Padding(
+        padding: const EdgeInsets.symmetric(vertical: 8.0),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Text(
+              title,
+              style: TextStyle(
+                fontSize: 18,
+                fontWeight: FontWeight.w500,
+                color: Colors.grey[600],
+              ),
+            ),
+            Icon(
+              Icons.arrow_forward_ios,
+              color: Colors.grey,
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
+
+//TODO password reset view
